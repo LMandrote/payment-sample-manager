cb3df56e9dfe2577ab4202c3828a4a824598e948
LMandrote <mandrotelucas@gmail.com>
2026-08-26 16:14:56 -0300

    Initial payment manager project

---
+++ src/app.js
@@ -0,0 +1,83 @@
+//Criando um objeto
+const pagamento = {
+    cliente: "Alpha Tech Ltda",
+    fornecedor: "Adecco",
+    invoice: "254",
+    valor: 5400.50,
+    vencimento: new Date(2026, 7, 26),
+    status: "Pago",
+}
+//console.log(pagamento);
+
+//Criando um array
+const pagamentos = [
+    {
+        cliente: "Argo Sementes Ltda",
+        fornecedor: "Ifood Benefícios",
+        invoice: "4df56",
+        valor: 54250.90,
+        vencimento: new Date(2026, 7, 31),
+        status: "Agendado",
+    },
+    {
+        cliente: "Alpha Tech Ltda",
+        fornecedor: "Adecco",
+        invoice: "254",
+        valor: 5400.50,
+        vencimento: new Date(2026, 7, 26),
+        status: "Pago",
+    },
+    {
+        cliente: "UbiSoni Jogos Digitais Ltda.",
+        fornecedor: "Cabum Eletrônicos",
+        invoice: "67",
+        valor: 4212.65,
+        vencimento: new Date(2026, 7, 25),
+        status: "Pendente",
+    }
+];   
+
+//Monstrando no terminal informações dos objetos inseridos dentro do Array
+//console.log(pagamentos[2].cliente);
+//console.log(pagamentos[0].valor);
+//console.log(pagamentos.length);
+
+let pendentes = 0;
+let agendados = 0;
+let pagos = 0;
+let valorTotal = 0;
+let valorPendente = 0;
+let valorAgendado = 0;
+let valorPago = 0;
+
+//Percorrendo os Arrays
+for(let i = 0; i < pagamentos.length; i++) {
+    console.log("Cliente: ", pagamentos[i].cliente);
+    console.log("Status: ", pagamentos[i].status);
+    console.log("Valor: ", pagamentos[i].valor);
+
+    if (pagamentos[i].status === "Pendente") {
+        pendentes++;
+        valorTotal = valorTotal + pagamentos[i].valor;
+        valorPendente = pagamentos[i].valor;
+
+    } else if (pagamentos[i].status === "Agendado") {
+        agendados++;
+        valorTotal = valorTotal + pagamentos[i].valor;
+        valorAgendado = pagamentos[i].valor;
+
+    } else { 
+        pagos++;
+        valorTotal = valorTotal + pagamentos[i].valor;
+        valorPago = pagamentos[i].valor;
+    }
+
+}
+    console.log("Pagamentos pendentes: ", pendentes, "| R$ ", valorPendente);
+    console.log("Pagamentos Agendados: ", agendados, "| R$ ", valorAgendado);
+    console.log("Pagamentos Pagos: ", pagos, "| R$ ", valorPago);
+
+    console.log("Valor total: ", valorTotal);
+
+
+
